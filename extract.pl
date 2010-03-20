389515b9c762cd89a8c2bf022a4a764e54f4fb0c
Stefan Kangas <skangas@skangas.se>
2010-03-20 22:36:04 +0100

    initial import

---
+++ extract.pl
@@ -0,0 +1,150 @@
+#!/usr/bin/perl
+package main;
+
+use 5.010;
+use autodie;
+use strict;
+use warnings;
+        
+use FindBin qw($Bin);
+use lib "$Bin/lib";
+
+use Archive::Extract;
+use File::Basename;
+use File::Finder;
+use Path::Class qw(file);
+use Cwd qw(cwd abs_path);
+use Data::Dumper;
+use Getopt::Long;
+use YAML::Syck;
+
+our $VERSION = '0.01';
+our $log;
+
+### COMMAND LINE OPTIONS
+our $conf = LoadFile('/home/skangas/code/Scextract/scextract.yml');
+GetOptions(
+    'delete'   => \$conf->{delete},
+    'debug'    => \$conf->{debug},
+    'pretend'  => \$conf->{pretend},
+    'quiet'    => \$conf->{quiet},
+#    'rename'   => \$conf->{rename},
+    'target=s' => \$conf->{target},
+    'verbose'  => \$conf->{verbose},
+#    'yes|y'    => \$conf->{yes},
+) or die "Unable to get command line options.";
+
+my $all_rars = File::Finder->type('f')->eval(sub{
+    ### find the type of file we want
+    /(?:\.part0*1.rar # 1  part01.rar
+     |
+         
+         (?:
+             (?:[^p][^a][^r][^t]\d+)
+         |
+             \D
+         )
+         \.rar    # 2  rar
+     |\.001           # 3  001
+     )\Z/ix;
+});
+# Matches: tdf-ptaa.part11.rar
+#          tcg.cd1-iffm.part39.rar
+
+    {
+        if ($conf->{debug}) {
+            $all_rars = $all_rars->print;
+        }
+        unless ($conf->{pretend}) {
+            my @command = qw'unrar -o+ -inul';
+            #    push @command, "-inul" unless $conf->{verbose};
+            push @command, 'x';
+            push @command, '{}';
+            push @command, $conf->{target} if $conf->{target};            
+            $all_rars = $all_rars->exec(@command);
+        }
+    }
+
+### Decide directories to work on
+my @search_dirs;
+if (scalar @ARGV) {
+    @search_dirs = @ARGV;
+} else {
+    push @search_dirs, cwd();
+}
+
+my @files = $all_rars->in(@search_dirs);
+
+### Delete the files
+
+sleep 1 unless $conf->{pretend};
+
+dprint( "\n###### REMOVED FILES ######\n" );
+my $rar_suffix = qr/\.(?:rar|r\d\d|\d{3}])\Z/i;
+
+for my $file (@files) {
+    my $basename = fileparse($file, $rar_suffix);
+    my $back = cwd();
+    my $all_volumes = File::Finder->type('f')->eval(sub {
+        /$basename$rar_suffix/
+    });
+    if ($conf->{debug}) {
+        $all_volumes = $all_volumes->print;
+    }
+    if ( $conf->{delete} && !$conf->{pretend}) {
+        $all_volumes = $all_volumes->eval(sub{ unlink $_; });
+    }
+    $all_volumes->in(file($file)->dir);
+    
+}
+
+##
+## VARIOUS SUBROUTINES
+##
+
+sub dprint {
+    if ( $conf->{debug} ) {
+        print for @_;
+    }
+}
+
+sub vprint {
+    if ( $conf->{verbose} || $conf->{debug} ) {
+        print for @_;
+    }
+}
+
+# TODO
+
+# - option to check sfv with default behaviour to do so upon validating release (sfv_check 0|1)
+# - option to check sfv only if extract failed - we might have incomplete downloads (sfv_check 2)
+# - option not valid release if there isn't an SFV (sfv_required 1|0)
+
+# - Properly handle Sub-directories
+
+# - handle this with more than just luck:
+#   Ronny.Och.Ragge.Tillbaka.Till.Farrden.1.VHSRiP.SVCD.SWEDiSH-RULLSTOL/
+#   Ronny.Och.Ragge.Tillbaka.Till.Farrden.2.VHSRiP.SVCD.SWEDiSH-RULLSTOL/
+#                                         ^ 36 chars, here be luck
+# - modularize codebase for later insertion into rtorrent-helper
+
+# - get a large number of release names and check them for sanity,
+#   this would involve writing a testcase
+
+# - FATX::Rename (or something)
+#   42 bytes ASCII
+#   2 gb file size
+#   should be able to take a list of regexp, i.e. [qr//, qr//] and use them for removing not needed information
+#   such as group tags and so on
+
+# - break of unraring into a module which should be suitable
+#   for insertion into CPAN
+
+# - determine what log modules is nice and use it (Log::Trivial?)
+#   instead of ugly hack (Log4Perl is better?)
+
+# - automatic sorting into directories based on regexps
+# - automatic ftp upload - Net::FTP? - is this suited for
+#   scextract or better left to another tool? shellscripting?
+
+1;
