4b8a7e1de60682ee9e5749cae431b0a8dd58f50a
Stefan Kangas <skangas@skangas.se>
2012-01-04 15:51:15 +0100

    Delete the files immediately after extracting them

--- extract.pl
+++ extract.pl
@@ -14,7 +14,10 @@ use File::Find::Rule;
 use Path::Class qw(file);
 use YAML::Syck;
 
-### COMMAND LINE OPTIONS
+###
+### PARSE OPTIONS
+###
+
 our $conf = LoadFile("$Bin/extract.yml");
 GetOptions(
     'delete'   => \$conf->{delete},
@@ -23,64 +26,6 @@ GetOptions(
     'target=s' => \$conf->{target},
     'verbose'  => \$conf->{verbose},
 ) or die "Unable to get command line options.";
-
-### If there were no directories specified, use the cwd
-my @search_dirs;
-if (scalar @ARGV) {
-    @search_dirs = @ARGV;
-} else {
-    push @search_dirs, cwd();
-}
-
-### Build the extract command
-my $find = File::Find::Rule->new;
-$find->file
-    ->nonempty
-    ->exec(sub {
-               m{\.001\Z}
-            || m{\.part0*1\.rar\Z}i
-            || m{\.rar\Z}i && !m{\.part\d+\.rar\Z}i;
-           });
-
-### Print the files
-vprint("Will extract:");
-my @files;
-@files = $find->in(@search_dirs);
-say for @files;
-vprint("");
-
-### Extract the files
-
-my @command = qw'unrar -o+ -c- -inul x';
-say "Now extracting:";
-for my $file (@files) {
-    say $file;
-    unless ($conf->{pretend}) {
-        my @cmd = (@command, $file);
-        push @cmd, $conf->{target} if $conf->{target};
-        system(@cmd);
-    }
-}
-
-### Delete the files
-exit unless $conf->{delete};
-
-say "";
-vprint( "Removing files..." );
-
-my $command = 'unrar -c- -v l';
-
-for my $file (@files) {
-    my $out;
-    open $out, "$command $file |";
-    my @volumes;
-    for (<$out>) {
-        if (/^Volume (.*)$/) {
-            push @volumes, $1;
-        }
-    }
-    say for @volumes;
-}
 
 ##
 ## VARIOUS SUBROUTINES
@@ -91,3 +36,65 @@ sub vprint {
         say for @_;
     }
 }
+
+sub delete_files {
+    my ($file) = @_;
+    my $command = 'unrar -c- -v l';
+    open my $out, "$command $file |";
+    my @volumes;
+    for (<$out>) {
+        if (/^Volume (.*)$/) {
+            push @volumes, $1;
+        }
+    }
+    unlink for @volumes;
+}
+
+##
+## MAIN PROGRAM
+##
+
+### If there were no directories specified, use the cwd
+my @search_dirs;
+if (scalar @ARGV) {
+    @search_dirs = @ARGV;
+} else {
+    push @search_dirs, cwd();
+}
+
+### Find all .rar files
+my $find = File::Find::Rule->new;
+$find->file
+    ->nonempty
+    ->exec(sub {
+               m{\.001\Z}
+            || m{\.part0*1\.rar\Z}i
+            || m{\.rar\Z}i && !m{\.part\d+\.rar\Z}i;
+           });
+my @files = $find->in(@search_dirs);
+
+### If pretending, print files and exit
+if ($conf->{pretend}) {
+    vprint("Pretending...");
+    say for @files;
+    exit;
+}
+
+### Extract the files
+my @command = qw'unrar -o+ -c- -inul x';
+for my $file (@files) {
+    print $file;
+    unless ($conf->{pretend}) {
+        my @cmd = (@command, $file);
+        push @cmd, $conf->{target} if $conf->{target};
+        system(@cmd);
+    }
+
+    ### Delete the files
+    if ($conf->{delete}) {
+        delete_files($file);
+    }
+
+    print " DONE\n";
+}
+
